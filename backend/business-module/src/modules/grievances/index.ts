064ce481979665d08b24f7f942fbb221b168aa1c
Kowsalya-devs <abikowsalya27@gmail.com>
2026-07-15 21:22:15 +0530

    Mount customer-facing grievances router in app.ts

---
+++ backend/business-module/src/modules/grievances/index.ts
@@ -0,0 +1,5 @@
+// src/modules/grievances/index.ts
+export { grievancesRouter } from './grievances.routes';
+export { grievancesService } from './grievances.service';
+export { grievancesRepository } from './grievances.repository';
+export type { Grievance } from './grievances.repository';
